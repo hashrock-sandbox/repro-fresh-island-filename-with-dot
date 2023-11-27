dc57ccce45b57df8d0a0e879c96023879419d8df
hashrock <hashrock@users.noreply.github.com>
2023-11-27 18:30:56 +0900

    fix

--- routes/foo.bar.tsx
+++ routes/foo.bar.tsx
@@ -1,16 +1,18 @@
 // Document https://fresh.deno.dev/docs/getting-started/create-a-route
 
+import { useSignal } from "@preact/signals";
 import { FooBar } from "../components/Foo.bar.tsx";
 import FooCounter from "../islands/FooCounter.bar.tsx";
 
 export default function Page() {
+  const count = useSignal(3);
   return (
     <main>
       <h1>Foo.bar</h1>
 
       <FooBar />
 
-      <FooCounter count={0} />
+      <FooCounter count={count} />
     </main>
   );
 }
